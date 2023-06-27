8856699488f7478c6dadf7bfe3ed393389dccd12
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-27 21:41:15 +0000

    Turn person class to nameable with correct_name method

--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
-class Person
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -16,6 +17,10 @@ class Person
   # age or if they have permission from parents.
   def can_use_services
     of_age || parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
