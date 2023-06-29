2e319596fa976353bb0a60ed0a5ece33992966c7
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-29 00:11:41 +0000

    Add rented_by and add_rental method

--- book.rb
+++ book.rb
@@ -2,7 +2,22 @@ class Book
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
   end
 
   attr_accessor :title, :author
+
+  # Add a rental to the book's rentals array
+  def add_rental(rental)
+    rentals << rental
+  end
+
+  # Get the people who rented the book
+  def rented_by
+    rentals.map(&:person)
+  end
+
+  private
+
+  attr_accessor :rentals
 end
