1c9b8711e46925f23ea9c04694ef9463c94e393b
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-07 21:47:24 +0000

    Add fizzbuzz method

--- tdd_activity/solver.rb
+++ tdd_activity/solver.rb
@@ -15,4 +15,12 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
