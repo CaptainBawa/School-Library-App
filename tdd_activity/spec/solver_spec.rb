c6e7bfc4503d245338788fe46fd7c5be7eb1e6f5
Adams-Ghub <bamfoadamsf@gmail.com>
2023-07-07 13:52:37 +0000

    Create TDD test for fatorial method

---
+++ tdd_activity/spec/solver_spec.rb
@@ -0,0 +1,13 @@
+require './solver'
+
+describe Solver do
+    subject {Solver.new}
+
+    it "calculate factorial of a number" do
+        expect(subject.factorial(4)).to eq(24)
+    end
+
+    it "throws an error for negative numbers" do
+        expect(subject.factorial(-1)).to eq "no negative number"
+    end 
+end
